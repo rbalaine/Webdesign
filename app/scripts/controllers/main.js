4091937876c68e8a0ed547444f1d610ad7f0fc90
Romain <romain.balaine@epfedu.fr>
2014-05-22 14:52:44 +0200

    Travail graphique et page about

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('webdesignApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+  var getdata=function(){
+		$http.get("http://public.opendatasoft.com/api/records/1.0/search?dataset=liste-des-boites-aux-lettres-paris&rows=1838&facet=type&facet=commune").success(function(data){
+  		console.log(JSON.stringify(data));
+  		$scope.data = data;
+  	});}
+
+		
+
+  	$http.get("http://public.opendatasoft.com/api/records/1.0/search?dataset=liste-des-boites-aux-lettres-paris&rows=1838&facet=type&facet=commune").success(function(data2){
+  		console.log(JSON.stringify(data2));
+  		$scope.data2 = data2;
+  	});
+
+  	getdata();
+
   });
